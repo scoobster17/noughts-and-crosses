ca09c249e4e50d7994f4613fef87b22f03eb89ea
Phil Gibbins <scoobster17@hotmail.co.uk>
2016-11-06 21:59:04 +0000

    Initial commit with basic game html and functionality

---
+++ README.md
@@ -0,0 +1,8 @@
+# Noughts and Crosses
+
+A simple noughts and crosses / tic-tac-toe game made form JavaScript, just for
+the hell of it.
+
+Notes:
+
+- constructor / prototype can be used if gets more complicated
---
+++ app/js/noughts-and-crosses.js
@@ -0,0 +1,88 @@
+;"use strict";
+
+window.nac = (function() {
+
+	// game config
+	var config = {
+		currentPlayer: null,
+		playerDetails: [
+			{
+				name: 'Player 1',
+				wins: 0,
+				type: 'noughts'
+			},
+			{
+				name: 'Player 2',
+				wins: 0,
+				type: 'crosses'
+			}
+		]
+	};
+
+	// element store
+	var elems = {};
+
+	var bindElements = function() {
+		elems.form = document.querySelector('form');
+		elems.turnIndicator = document.getElementById('turn-indicator');
+		elems.gameTable = document.getElementById('game-table');
+		elems.cells = elems.gameTable.querySelectorAll('td');
+	};
+
+	var bindEvents = function() {
+		elems.form.addEventListener('submit', function(event) {
+			event.preventDefault();
+			setupGame();
+			return false;
+		});
+		elems.cells.forEach(function(element) {
+			element.addEventListener('click', function(event) {
+				var type = config.playerDetails[config.currentPlayer - 1].type;
+				joose.utils.addClass(event.target, type);
+				event.target.innerHTML = type;
+				endTurn();
+				checkForWin();
+				startTurn();
+			});
+		});
+	};
+
+	var startTurn = function( ){
+		var playerName = config.playerDetails[config.currentPlayer - 1].name;
+		document.title = playerName + '\'s turn - Noughts and crosses';
+		elems.turnIndicator.innerHTML = playerName;
+	}
+	var endTurn = function() {
+		setNextPlayer();
+	}
+	var checkForWin = function() {
+		// check for win
+	};
+
+	var setNextPlayer = function() {
+		config.currentPlayer = (config.currentPlayer === 1) ? 2 : 1;
+	}
+
+	var setupGame = function() {
+		//clearPreviousGame();
+		joose.utils.addClass(elems.form, 'visually-hidden');
+		//getPlayerDetails();
+		joose.utils.removeClass(elems.gameTable, 'visually-hidden');
+		setNextPlayer();
+		startTurn();
+	};
+
+	// private methods
+	var init = function() {
+		bindElements();
+		bindEvents();
+	}
+
+	// public methods
+	return {
+		init: init
+	};
+
+})();
+
+nac.init();
